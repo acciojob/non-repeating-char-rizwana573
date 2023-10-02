ee7c944d9ad746661aa44207cbeda5408e3ab428
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-02 11:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++){
+		let j= str.charAt(i);
+		if(str.indexOf(j) == str.lastIndexOf(j)){
+			return j;
+		}
+	} 
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
